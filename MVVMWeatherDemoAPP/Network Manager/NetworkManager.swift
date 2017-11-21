8581730b0826b4a8739826651dbef143f532dcf7
dennisvera <dennisvera.z@gmail.com>
2017-11-20 20:51:04 -0500

    network manager class created

---
+++ MVVMWeatherDemoAPP/Network Manager/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  MVVMWeatherDemoAPP
+//
+//  Created by Dennis Vera on 11/20/17.
+//  Copyright © 2017 Dennis Vera. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    
+}
