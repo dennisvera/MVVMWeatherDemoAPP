d696198f362ca7c1a4acb6fba5768421d156d7c6
dennisvera <dennisvera.z@gmail.com>
2017-12-02 17:06:56 -0500

    weatherDayCell class created

---
+++ MVVMWeatherDemoAPP/View Controllers/Weather View Controller/Table View Cells/WetaherDayTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  WetaherDayTableViewCell.swift
+//  MVVMWeatherDemoAPP
+//
+//  Created by Dennis Vera on 12/2/17.
+//  Copyright © 2017 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDayTableViewCell: UITableViewCell {
+    
+    // MARK: - Type Properties
+    
+    static let reuseIdentifier = "WeatherDayCell"
+    
+    // MARK: - Properties
+    
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var windSpeedLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var iconImageView: UIImageView!
+    
+    // MARK: - Initialization
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Configure Cell
+        selectionStyle = .none
+    }
+}
