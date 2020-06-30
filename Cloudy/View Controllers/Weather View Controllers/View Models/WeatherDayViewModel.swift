73e9743f879e5d07cc4865723b31cb4501b7b3e0
DennisVera <dennisvera.z@gmail.com>
2020-06-30 10:47:17 -0400

    Crreate unit test for the WeatherDayViewModel

--- Cloudy/View Controllers/Weather View Controllers/View Models/WeatherDayViewModel.swift
+++ Cloudy/View Controllers/Weather View Controllers/View Models/WeatherDayViewModel.swift
@@ -9,66 +9,64 @@
 import UIKit
 
 struct WeatherDayViewModel {
-
-    // MARK: - Properties
-
-    let weatherDayData: WeatherDayData
-
-    // MARK: -
-
-    private let dateFormatter = DateFormatter()
-
-    // MARK: -
-
-    var day: String {
-        // Configure Date Formatter
-        dateFormatter.dateFormat = "EEEE"
-
-        return dateFormatter.string(from: weatherDayData.time)
+  
+  // MARK: - Properties
+  
+  let weatherDayData: WeatherDayData
+  
+  // MARK: -
+  
+  private let dateFormatter = DateFormatter()
+  
+  // MARK: -
+  
+  var day: String {
+    // Configure Date Formatter
+    dateFormatter.dateFormat = "EEEE"
+    
+    return dateFormatter.string(from: weatherDayData.time)
+  }
+  
+  var date: String {
+    // Configure Date Formatter
+    dateFormatter.dateFormat = "MMMM d"
+    
+    return dateFormatter.string(from: weatherDayData.time)
+  }
+  
+  var temperature: String {
+    let min = format(temperature: weatherDayData.temperatureMin)
+    let max = format(temperature: weatherDayData.temperatureMax)
+    
+    return "\(min) - \(max)"
+  }
+  
+  var windSpeed: String {
+    let windSpeed = weatherDayData.windSpeed
+    
+    switch UserDefaults.unitsNotation {
+    case .imperial:
+      return String(format: "%.f MPH", windSpeed)
+    case .metric:
+      return String(format: "%.f KPH", windSpeed.toKPH)
     }
-
-    var date: String {
-        // Configure Date Formatter
-        dateFormatter.dateFormat = "MMMM d"
-
-        return dateFormatter.string(from: weatherDayData.time)
+  }
+  
+  var image: UIImage? {
+    UIImage.imageForIcon(with: weatherDayData.icon)
+  }
+  
+  // MARK: - Helper Methods
+  
+  private func format(temperature: Double) -> String {
+    switch UserDefaults.temperatureNotation {
+    case .fahrenheit:
+      return String(format: "%.0f °F", temperature)
+    case .celsius:
+      return String(format: "%.0f °C", temperature.toCelcius)
     }
-
-    var temperature: String {
-        let min = format(temperature: weatherDayData.temperatureMin)
-        let max = format(temperature: weatherDayData.temperatureMax)
-
-        return "\(min) - \(max)"
-    }
-
-    var windSpeed: String {
-        let windSpeed = weatherDayData.windSpeed
-
-        switch UserDefaults.unitsNotation {
-        case .imperial:
-            return String(format: "%.f MPH", windSpeed)
-        case .metric:
-            return String(format: "%.f KPH", windSpeed.toKPH)
-        }
-    }
-
-    var image: UIImage? {
-        UIImage.imageForIcon(with: weatherDayData.icon)
-    }
-
-    // MARK: - Helper Methods
-
-    private func format(temperature: Double) -> String {
-        switch UserDefaults.temperatureNotation {
-        case .fahrenheit:
-            return String(format: "%.0f °F", temperature)
-        case .celsius:
-            return String(format: "%.0f °C", temperature.toCelcius)
-        }
-    }
-
+  }
+  
 }
 
-extension WeatherDayViewModel: WeatherDayPresentable {
-
-}
+extension WeatherDayViewModel: WeatherDayPresentable {}
---
+++ CloudyTests/Test Cases/WeekViewModelTests.swift
@@ -0,0 +1,60 @@
+//
+//  WeekViewModelTests.swift
+//  CloudyTests
+//
+//  Created by Dennis Vera on 6/30/20.
+//  Copyright © 2020 Cocoacasts. All rights reserved.
+//
+
+import XCTest
+@testable import Cloudy
+
+class WeekViewModelTests: XCTestCase {
+  
+  // MARK: - Properties
+  
+  var viewModel: WeekViewModel!
+  
+  // MARK: - Set Up & Tear Down
+  
+  override func setUpWithError() throws {
+    // Load Stub
+    let data = loadStub(name: "weather", extension: "json")
+    
+    // Create JSON Decoder
+    let decoder = JSONDecoder()
+    
+    // Configure JSON Decoder
+    decoder.dateDecodingStrategy = .secondsSince1970
+    
+    // Decode JSON
+    let weatherData = try decoder.decode(WeatherData.self, from: data)
+    
+    // Initialize View Model
+    viewModel = WeekViewModel(weatherData: weatherData.dailyData)
+  }
+  
+  override func tearDownWithError() throws {
+  }
+  
+  // MARK: - Tests for Number of Sections
+  
+  func testNumberOfSections() {
+    XCTAssertEqual(viewModel.numberOfSections, 1)
+  }
+  
+  // MARK: - Tests for Number of Days
+  
+  func testNumberOfDays() {
+    XCTAssertEqual(viewModel.numberOfDays, 8)
+  }
+  
+  // MARK: - Tests for View Model for Index
+  
+  func testViewModelForIndex() {
+    let weatherdayViewModel = viewModel.viewModel(for: 5)
+    
+    XCTAssertEqual(weatherdayViewModel.day, "Friday")
+    XCTAssertEqual(weatherdayViewModel.date, "June 26")
+  }
+}
